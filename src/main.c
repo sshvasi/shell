d6d061bebd91be8f9ca54e892938b86555873332
Alexander Vasiliev <asvasi25@gmail.com>
2023-12-06 04:43:42 +0400

    Add linked list

--- src/main.c
+++ src/main.c
@@ -5,15 +5,20 @@
 
 #define BUFFER_SIZE 64
 
-int main(void)
-{
-    char buffer[BUFFER_SIZE];
+struct word_item {
     char *word;
+    struct word_item *next;
+};
+
+int main(void) {
+    char buffer[BUFFER_SIZE];
+
+    struct word_item *first_word;
+    struct word_item *new_word;
+    struct word_item *tmp_word;
 
     int word_index;
     int buff_index;
-
-    bool space;
 
     while (true) {
         fputs("> ", stdout);
@@ -28,40 +33,48 @@ int main(void)
             }
         }
 
-        for (buff_index = word_index = 0,
-             space = false,
-             word = malloc(BUFFER_SIZE);
+        new_word = malloc(sizeof(struct word_item));
+        new_word->word = malloc(BUFFER_SIZE);
+        new_word->next = NULL;
+        first_word = new_word;
+
+        for (buff_index = word_index = 0;
              buff_index < strlen(buffer);
              buff_index++) {
             switch (buffer[buff_index]) {
                 case '\n':
-                    word[word_index] = '\0';
-                    word_index = 0;
-                    space = false;
-                    printf("[%s]\n", word);
-                    free(word);
-                    word = malloc(BUFFER_SIZE);
-                    break;
                 case '\t':
                 case ' ':
-                    if (!space) {
-                        word[word_index] = '\0';
-                        word_index = 0;
-                        space = true;
-                        printf("[%s]\n", word);
-                        free(word);
-                        word = malloc(BUFFER_SIZE);
+                    if (word_index == 0) {
+                        break;
                     }
+
+                    first_word->word[word_index] = '\0';
+                    word_index = 0;
+
+                    new_word = malloc(sizeof(struct word_item));
+                    new_word->word = malloc(BUFFER_SIZE);
+                    new_word->next = first_word;
+                    first_word = new_word;
                     break;
                 default:
-                    if (space)
-                        space = false;
-                    word[word_index++] = buffer[buff_index];
-                    break;
+                    first_word->word[word_index++] = buffer[buff_index];
             }
+        }
+
+        while (first_word) {
+            if (strlen(first_word->word) > 0) {
+                printf("[%s]\n", first_word->word);
+            } else {
+                puts("<empty word>");
+            }
+
+            tmp_word = first_word->next;
+            free(first_word->word);
+            free(first_word);
+            first_word = tmp_word;
         }
     }
 
-    free(word);
     exit(EXIT_SUCCESS);
 }
