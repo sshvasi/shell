b4d62b2fc75d6ce603886aeeef0bf5271139405c
Alexander Vasiliev <asvasi25@gmail.com>
2023-12-06 07:20:25 +0400

    Use FSM

--- src/main.c
+++ src/main.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 
 #define BUFFER_SIZE 64
 
-struct node {
-    char *word;
-    struct node *next;
-};
+enum state { normal, quote, escape };
+
+struct node { char *word; struct node *next; };
 
 static struct node *init_list(void);
 static void add_to_list(struct node **tail);
-static void term_list(struct node*);
+static void term_list(struct node*, bool);
+
+bool isdoublequote(int ch) { return ch == '"'; }
+bool isescape(int ch) { return ch == '\\'; }
 
 int main(void) {
     char buffer[BUFFER_SIZE];
-
-    struct node *first_word;
-    struct node *last_word;
-
-    int word_index;
-    int buff_index;
+    struct node *first_word, *last_word;
+    int word_index, buff_index;
+    enum state curr_state;
 
     while (true) {
         fputs("> ", stdout);
@@ -29,36 +29,60 @@ int main(void) {
 
         if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) {
             if (feof(stdin)) {
+                term_list(first_word, true);
                 exit(EXIT_SUCCESS);
             } else {
                 fputs("Cannot read a line.\n", stderr);
+                term_list(first_word, true);
                 exit(EXIT_FAILURE);
             }
         }
 
-        for (first_word = last_word = init_list(),
-             buff_index = word_index = 0;
-             buff_index < strlen(buffer);
+        first_word = last_word = init_list();
+        curr_state = normal;
+        word_index = 0;
+
+        for (buff_index = 0;
+             buff_index < BUFFER_SIZE && buffer[buff_index];
              buff_index++) {
-            switch (buffer[buff_index]) {
-                case '\n':
-                case '\t':
-                case ' ':
-                    if (word_index == 0) {
-                        break;
+            char ch = buffer[buff_index];
+
+            switch (curr_state) {
+                case normal:
+                    if (isdoublequote(ch)) {
+                        curr_state = quote;
+                    } else if (!isspace(ch)) {
+                        last_word->word[word_index++] = ch;
+                    } else if (word_index > 0) {
+                        last_word->word[word_index] = '\0';
+                        word_index = 0;
+                        add_to_list(&last_word);
                     }
-
-                    last_word->word[word_index] = '\0';
-                    word_index = 0;
-
-                    add_to_list(&last_word);
+                    break;
+                case quote:
+                    if (isdoublequote(ch)) {
+                        curr_state = normal;
+                    } else if (isescape(ch)) {
+                        curr_state = escape;
+                    } else {
+                        last_word->word[word_index++] = ch;
+                    }
+                    break;
+                case escape:
+                    last_word->word[word_index++] = ch;
+                    curr_state = quote;
                     break;
                 default:
-                    last_word->word[word_index++] = buffer[buff_index];
+                    break;
             }
         }
 
-        term_list(first_word);
+        if (word_index > 0) {
+            last_word->word[word_index] = '\0';
+            add_to_list(&last_word);
+        }
+
+        term_list(first_word, curr_state == normal);
     }
 
     exit(EXIT_SUCCESS);
@@ -83,13 +107,17 @@ static void add_to_list(struct node **tail)
     *tail = new_tail;
 }
 
-static void term_list(struct node *head)
+static void term_list(struct node *head, bool success)
 {
+    if (!success) {
+        fputs("Syntax error. Incorrect number of quotes.\n", stderr);
+    }
+
     while (head) {
-        if (strlen(head->word) > 0) {
-            printf("[%s]\n", head->word);
-        } else {
-            puts("<empty word>");
+        if (success) {
+            if (strlen(head->word) > 0) {
+                printf("[%s]\n", head->word);
+            }
         }
 
         struct node *tmp = head->next;
