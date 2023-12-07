736a04e83f201a2fe83c00e9e74adf987ba045b8
Alexander Vasiliev <asvasi25@gmail.com>
2023-12-07 21:21:05 +0400

    Remove switch-cases

--- src/repl.c
+++ src/repl.c
@@ -5,20 +5,8 @@
 
 #define BUFFER_SIZE 64
 
-enum state {
-    normal,
-    quote,
-    escape
-};
-
 struct context {
     char buffer[BUFFER_SIZE];
-
-    int prev_ch;
-    int curr_ch;
-
-    enum state prev_st;
-    enum state curr_st;
 
     struct node *first_nd;
     struct node *last_nd;
@@ -27,13 +15,49 @@ struct context {
     int buff_idx;
 } ctx;
 
-static void repl_print_prompt()
+enum state {
+    normal,
+    quote,
+    escape
+} prev_st = normal, curr_st = normal;
+
+enum event {
+    quote_ch = '"',
+    escape_ch = '\\',
+    space_ch = ' ',
+    tab_ch = '\t',
+    nl_ch = '\n'
+    /* default = any character */
+} prev_ev, curr_ev;
+
+typedef enum state (*processor)(enum event);
+
+static void repl_print_prompt(void);
+static void repl_read_line(void);
+static void repl_parse_line(void);
+
+static void process(enum event ev);
+
+static enum state process_normal(enum event ev);
+static enum state process_quote(enum event ev);
+static enum state process_escape(enum event ev);
+
+void repl_loop()
+{
+    while (true) {
+        repl_print_prompt();
+        repl_read_line();
+        repl_parse_line();
+    }
+}
+
+static void repl_print_prompt(void)
 {
     fputs("> ", stdout);
     fflush(stdout);
 }
 
-static void repl_read_line()
+static void repl_read_line(void)
 {
     if (fgets(ctx.buffer, BUFFER_SIZE, stdin) == NULL) {
         if (feof(stdin)) {
@@ -45,77 +69,15 @@ static void repl_read_line()
     }
 }
 
-static void repl_to_state(enum state st)
-{
-    ctx.prev_st = ctx.curr_st;
-    ctx.curr_st = st;
-}
-
-static void repl_process_normal(int ch)
-{
-    if (ctx.curr_ch == '"') {
-        repl_to_state(quote);
-    } else if (ctx.curr_ch == '\\') {
-        repl_to_state(escape);
-    } else if (!isspace(ctx.curr_ch)) {
-        ctx.last_nd->word[ctx.word_idx++] = ctx.curr_ch;
-    } else if (ctx.word_idx > 0) {
-        ctx.last_nd->word[ctx.word_idx] = '\0';
-        ctx.word_idx = 0;
-        list_add(&ctx.last_nd);
-    }
-}
-
-static void repl_process_quote(int ch)
-{
-    if (ctx.curr_ch == '"') {
-        if (ctx.prev_ch == '"') {
-            ctx.last_nd->word[ctx.word_idx++] = ' ';
-        }
-        repl_to_state(normal);
-    } else if (ctx.curr_ch == '\\') {
-        repl_to_state(escape);
-    } else {
-        ctx.last_nd->word[ctx.word_idx++] = ctx.curr_ch;
-    }
-}
-
-static void repl_process_escape(int ch)
-{
-    ctx.last_nd->word[ctx.word_idx++] = ctx.curr_ch;
-    repl_to_state(ctx.prev_st);
-}
-
-static void repl_process(int ch)
-{
-    ctx.curr_ch = ch;
-    switch (ctx.curr_st) {
-        case normal:
-            repl_process_normal(ch);
-            break;
-        case quote:
-            repl_process_quote(ch);
-            break;
-        case escape:
-            repl_process_escape(ch);
-            break;
-        default:
-            break;
-
-    }
-    ctx.prev_ch = ctx.curr_ch;
-}
-
-static void repl_parse_line()
+static void repl_parse_line(void)
 {
     ctx.first_nd = ctx.last_nd = list_init();
-    ctx.curr_st = ctx.prev_st = normal;
     ctx.word_idx = 0;
 
     for (ctx.buff_idx = 0;
          ctx.buff_idx < BUFFER_SIZE && ctx.buffer[ctx.buff_idx];
          ctx.buff_idx++) {
-        repl_process(ctx.buffer[ctx.buff_idx]);
+        process(ctx.buffer[ctx.buff_idx]);
     }
 
     if (ctx.word_idx > 0) {
@@ -123,14 +85,75 @@ static void repl_parse_line()
         list_add(&ctx.last_nd);
     }
 
-    list_term(ctx.first_nd, ctx.curr_st == normal);
+    list_term(ctx.first_nd, curr_st == normal);
 }
 
-void repl_loop()
+static void process(enum event ev)
 {
-    while (true) {
-        repl_print_prompt();
-        repl_read_line();
-        repl_parse_line();
+    const processor processors[] = {
+        process_normal,
+        process_quote,
+        process_escape
+    };
+
+    prev_ev = curr_ev;
+    curr_ev = ev;
+
+    enum state tmp_st = curr_st;
+    curr_st = processors[curr_st](ev);
+    prev_st = tmp_st;
+}
+
+static enum state process_normal(enum event ev)
+{
+    switch (ev) {
+        case quote_ch:
+            return quote;
+        case escape_ch:
+            return escape;
+        case space_ch:
+        case tab_ch:
+        case nl_ch:
+            if (ctx.word_idx > 0) {
+                ctx.last_nd->word[ctx.word_idx] = '\0';
+                ctx.word_idx = 0;
+                list_add(&ctx.last_nd);
+            }
+            return normal;
+        default:
+            ctx.last_nd->word[ctx.word_idx++] = ev;
+            return normal;
+    }
+}
+
+static enum state process_quote(enum event ev)
+{
+    switch (ev) {
+        case quote_ch:
+            if (prev_ev == quote_ch)
+                ctx.last_nd->word[ctx.word_idx++] = ' ';
+            return normal;
+        case escape_ch:
+            return escape;
+        case space_ch:
+        case tab_ch:
+        case nl_ch:
+        default:
+            ctx.last_nd->word[ctx.word_idx++] = ev;
+            return quote;
+    }
+}
+
+static enum state process_escape(enum event ev)
+{
+    switch (ev) {
+        case quote_ch:
+        case escape_ch:
+        case space_ch:
+        case tab_ch:
+        case nl_ch:
+        default:
+            ctx.last_nd->word[ctx.word_idx++] = ev;
+            return prev_st;
     }
 }
